ca3d5d076ab44c54c6ccca6d8d5c1f73cb688486
Louis Royer <infos.louis.royer@gmail.com>
2025-08-22 19:25:00 +0200

    Move "run" to subcommand

--- main.go
+++ main.go
@@ -17,6 +17,16 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+func checkUnknownArgs(ctx *cli.Context) error {
+	if ctx.Args().Len() > 0 {
+		cli.ShowAppHelp(ctx)
+		logrus.WithFields(logrus.Fields{
+			"unknown-args": ctx.Args(),
+		}).Error("Unknown arguments")
+	}
+	return nil
+}
+
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
 	defer cancel()
@@ -27,28 +37,35 @@ func main() {
 		Authors: []*cli.Author{
 			{Name: "Louis Royer"},
 		},
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:     "uuid",
-				Usage:    "UUID of a karaoke to probe",
-				Required: false,
-			},
-		},
-		Action: func(ctx *cli.Context) error {
-			if err := app.NewSetup().Run(ctx.Context, ctx.String("uuid")); err != nil {
-				logrus.WithError(err).Fatal("Error while running, exiting…")
-			}
-			return nil
-		},
+
 		Commands: []*cli.Command{
 			{
-				Name:  "list-probes",
-				Usage: "info about available probes",
+				Name:   "list-probes",
+				Usage:  "info about available probes",
+				Before: checkUnknownArgs,
 				Action: func(ctx *cli.Context) error {
 					if err := app.NewListProbes().Run(ctx.Context); err != nil {
 						logrus.WithError(err).Fatal("Error while running, exiting…")
 					}
 					return nil
+				},
+			},
+			{
+				Name:   "run",
+				Usage:  "info about available probes",
+				Before: checkUnknownArgs,
+				Action: func(ctx *cli.Context) error {
+					if err := app.NewSetup().Run(ctx.Context, ctx.String("uuid")); err != nil {
+						logrus.WithError(err).Fatal("Error while running, exiting…")
+					}
+					return nil
+				},
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "uuid",
+						Usage:    "UUID of a karaoke to probe",
+						Required: false,
+					},
 				},
 			},
 		},
